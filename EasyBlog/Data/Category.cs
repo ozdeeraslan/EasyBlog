330a57b0c0553d9b201425997b0b033c7f9cbca7
ozdeeraslan <ozdenureraslan@gmail.com>
2024-02-07 08:19:44 +0100

    first migration and seed data added

---
+++ EasyBlog/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyBlog.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        // Navigation property
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
